a98fb4daed6ccda7d005d7233c9d99fd3a17c002
Przemek <przemek.nowak.pl@gmail.com>
2013-11-18 20:07:57 +0100

    NullObject Pattern

---
+++ src/main/java/pl/jvsystem/patterns/behavioral/nullobject/NullProduct.java
@@ -0,0 +1,16 @@
+package pl.jvsystem.patterns.behavioral.nullobject;
+
+/**
+ * Null Object.
+ *
+ * Przemek Nowak <przemek.nowak.pl@gmail.com>
+ * Date: 18.11.13 19:58
+ */
+public class NullProduct extends ProductBase
+{
+	@Override
+	public void rateIt(int value)
+	{
+		//nothing
+	}
+}
