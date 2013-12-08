cc8670b6bbfac38210ce79c61a4efd507ffc733a
Przemek <przemek.nowak.pl@gmail.com>
2013-12-08 13:12:19 +0100

    Redis recognition & watch service from java 7 example

---
+++ src/main/java/pl/jvsystem/java7/watchService/WatchDirectoryDemo.java
@@ -0,0 +1,26 @@
+package pl.jvsystem.java7.watchService;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * Przemek Nowak <przemek.nowak.pl@gmail.com>
+ * Date: 07.12.13 22:22
+ */
+public class WatchDirectoryDemo
+{
+	public static void main(String[] args)
+	{
+		final Path path = Paths.get("D:\\Dropbox");
+		WatchDirectory watch = new WatchDirectory();
+		try
+		{
+			watch.watchDir(path);
+		}
+		catch (IOException | InterruptedException ignored)
+		{
+
+		}
+	}
+}
