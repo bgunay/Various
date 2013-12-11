2c5a8cb539904618525cb1409b7c3cc2e59abfbd
nowakp1 <przemyslaw.nowak@contractors.roche.com>
2013-12-11 11:05:28 +0100

    SuperCSV test

---
+++ src/main/java/pl/jvsystem/csv/Demo.java
@@ -0,0 +1,23 @@
+package pl.jvsystem.csv;
+
+import org.supercsv.io.CsvBeanReader;
+import org.supercsv.io.ICsvBeanReader;
+import org.supercsv.prefs.CsvPreference;
+
+import java.io.FileReader;
+import java.util.Arrays;
+
+/**
+ * Date: 11.12.13 10:53
+ */
+public class Demo {
+
+	public static void main(String[] args) throws Exception {
+
+		try(ICsvBeanReader inFile = new CsvBeanReader(new FileReader("C:\\test1a.txt"), CsvPreference.EXCEL_PREFERENCE)) {
+			final String[] header = inFile.getHeader(true);
+			System.out.println("header: " + Arrays.toString(header));
+			System.out.println("header: " + header.length);
+		}
+	}
+}
