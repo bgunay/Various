0c68922b02f9285c054610e2048b3b4f5d3909f1
Przemek <przemek.nowak.pl@gmail.com>
2013-12-17 23:24:48 +0100

    Added logback logging

--- src/main/java/pl/jvsystem/guava/eventbus/EventBusDemo.java
+++ src/main/java/pl/jvsystem/guava/eventbus/EventBusDemo.java
@@ -1,5 +1,7 @@
 package pl.jvsystem.guava.eventbus;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import pl.jvsystem.guava.eventbus.events.CashPurchaseEvent;
 
 import com.google.common.eventbus.EventBus;
@@ -9,15 +11,15 @@ import pl.jvsystem.guava.eventbus.events.CreditPurchaseEvent;
  * Date: 16.12.13 18:26
  */
 public class EventBusDemo {
+	private static final Logger LOG = LoggerFactory.getLogger(EventBusDemo.class);
 
 	public static void main(String[] args) {
-
+		LOG.debug("startuję!");
 		EventBus eventBus = new EventBus();
 		eventBus.register(new PurchaseSubscriber());
 
 		eventBus.post(new CashPurchaseEvent("ItemOne", 35));
 		eventBus.post(new CreditPurchaseEvent("ItemTwo", 42, "0098329832"));
-
 	}
 
 }
