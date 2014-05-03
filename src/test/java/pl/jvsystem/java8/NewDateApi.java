1509b2d4f2b38179b6cfc87c23b34bbd10e97ad4
Przemek Nowak <przemyslaw.nowak@contractors.roche.com>
2014-05-03 18:30:39 +0200

    Java8

---
+++ src/test/java/pl/jvsystem/java8/NewDateApi.java
@@ -0,0 +1,116 @@
+package pl.jvsystem.java8;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.time.temporal.ChronoField;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+import java.util.Locale;
+
+/**
+ * Date: 2014-05-03 17:55
+ */
+public class NewDateApi {
+	private static final Logger LOG = LoggerFactory.getLogger(NewDateApi.class);
+
+	@Test
+	public void testClock() throws Exception {
+		Clock clock = Clock.systemDefaultZone();
+		long millis = clock.millis();
+
+		Instant instant = clock.instant();
+
+		Date legacyDate = Date.from(instant);   // legacy java.util.Date
+	}
+
+	@Test
+	public void testTimeZone() throws Exception {
+		LOG.trace("All zones: {}", ZoneId.getAvailableZoneIds());
+
+		ZoneId zone1 = ZoneId.of("Europe/Warsaw");
+		ZoneId zone2 = ZoneId.of("America/Los_Angeles");
+		System.out.println(zone1.getRules());
+		System.out.println(zone2.getRules());
+
+		LocalTime now1 = LocalTime.now(zone1);
+		LocalTime now2 = LocalTime.now(zone2);
+
+		System.out.println(now1.isBefore(now2));  // false
+
+		long hoursBetween = ChronoUnit.HOURS.between(now1, now2);
+		long minutesBetween = ChronoUnit.MINUTES.between(now1, now2);
+
+		System.out.println(hoursBetween);       // -3
+		System.out.println(minutesBetween);     // -239
+	}
+
+	@Test
+	public void testTime() throws Exception {
+		LocalTime late = LocalTime.of(23, 59, 59);
+
+		System.out.println(late);       // 23:59:59
+
+		DateTimeFormatter germanFormatter =
+				DateTimeFormatter
+						.ofLocalizedTime(FormatStyle.SHORT)
+						.withLocale(Locale.forLanguageTag("PL"));
+
+		LocalTime leetTime = LocalTime.parse("13:37", germanFormatter);
+		System.out.println(leetTime);   // 13:37
+
+	}
+
+	@Test
+	public void testLocalDate() throws Exception {
+		LocalDate today = LocalDate.now();
+		LocalDate tomorrow = today.plus(1, ChronoUnit.DAYS);
+		LocalDate yesterday = tomorrow.minusDays(2);
+
+		LocalDate independenceDay = LocalDate.of(1987, Month.MAY, 30);
+		DayOfWeek dayOfWeek = independenceDay.getDayOfWeek();
+		System.out.println(dayOfWeek);    // FRIDAY
+
+		DateTimeFormatter germanFormatter =
+				DateTimeFormatter
+						.ofLocalizedDate(FormatStyle.MEDIUM)
+						.withLocale(Locale.GERMAN);
+
+		LocalDate xmas = LocalDate.parse("24.12.2014", germanFormatter);
+		System.out.println(xmas);   // 2014-12-24
+	}
+
+	@Test
+	public void testLocalDateTime() throws Exception {
+		LocalDateTime sylvester = LocalDateTime.of(2014, Month.DECEMBER, 31, 23, 59, 59);
+
+		DayOfWeek dayOfWeek = sylvester.getDayOfWeek();
+		System.out.println(dayOfWeek);      // WEDNESDAY
+
+		Month month = sylvester.getMonth();
+		System.out.println(month);          // DECEMBER
+
+		long minuteOfDay = sylvester.getLong(ChronoField.MINUTE_OF_DAY);
+		System.out.println(minuteOfDay);    // 1439
+
+		Instant instant = sylvester
+				.atZone(ZoneId.systemDefault())
+				.toInstant();
+
+		Date legacyDate = Date.from(instant);
+		System.out.println(legacyDate);
+
+		DateTimeFormatter formatter =
+				DateTimeFormatter
+						.ofPattern("MMM dd, yyyy - HH:mm");
+
+
+		String string = formatter.format(LocalDateTime.now());
+		System.out.println(string);     // Nov 03, 2014 - 07:13
+
+	}
+}
--- src/test/java/pl/jvsystem/java8/StreamsTest.java
+++ src/test/java/pl/jvsystem/java8/StreamsTest.java
@@ -1,20 +1,110 @@
 package pl.jvsystem.java8;
 
-import java.util.Arrays;
+import com.google.common.base.Stopwatch;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.*;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Stream;
 
-import org.junit.Test;
+import static org.fest.assertions.Assertions.assertThat;
 
 
 /**
  * Date: 2014-05-02 21:26
  */
 public class StreamsTest {
+	private static final Logger LOG = LoggerFactory.getLogger(StreamsTest.class);
+	private List<String> stringCollection = new ArrayList<>();
+
+	@Before
+	public void setUp() throws Exception {
+		stringCollection.add("ddd2");
+		stringCollection.add("aaa2");
+		stringCollection.add("bbb1");
+		stringCollection.add("aaa1");
+		stringCollection.add("bbb3");
+		stringCollection.add("ccc");
+		stringCollection.add("bbb2");
+		stringCollection.add("ddd1");
+	}
 
 	@Test
 	public void streamingTest() {
 		Stream<Integer> ints = Arrays.asList(1, 2, 3, 4, 5).stream();
-		Stream<Integer> oddInts = ints.filter(n -> n%2 != 0);
+		Stream<Integer> oddInts = ints.filter(n -> n % 2 != 0);
 		oddInts.forEach(System.out::println);
 	}
+
+	@Test
+	public void testFilterStream() throws Exception {
+		stringCollection.stream().filter(o -> o.startsWith("a")).forEach(System.out::println);
+	}
+
+	@Test
+	public void testSortedStream() throws Exception {
+		stringCollection.stream().sorted().filter(o -> o.startsWith("a")).forEach(System.out::println);
+	}
+
+	@Test
+	public void testMap() throws Exception {
+		stringCollection.stream().map(String::toUpperCase).forEach(System.out::println);
+	}
+
+	@Test
+	public void testMatch() throws Exception {
+		boolean anyStartsWithA =
+				stringCollection
+						.stream()
+						.anyMatch((s) -> s.startsWith("a"));
+		assertThat(anyStartsWithA).isTrue();
+
+		boolean allStartsWithA =
+				stringCollection
+						.stream()
+						.allMatch((s) -> s.startsWith("a"));
+		assertThat(allStartsWithA).isFalse();
+	}
+
+	@Test
+	public void testCount() throws Exception {
+		long startsWithB =
+				stringCollection
+						.stream()
+						.filter((s) -> s.startsWith("b"))
+						.count();
+		assertThat(startsWithB).isEqualTo(3);
+	}
+
+	@Test
+	public void testReduce() throws Exception {
+		Optional<String> reduced = stringCollection.stream().sorted().reduce((s1, s2) -> s1 + "#" + s2);
+		reduced.ifPresent(System.out::println);
+	}
+
+	@Test
+	public void testSequentialAndParralerSort() throws Exception {
+		List<String> values = getUniqueList();
+		Stopwatch s = Stopwatch.createStarted();
+		values.stream().sorted().count();
+		LOG.info("T1: {} ms", s.elapsed(TimeUnit.MILLISECONDS));
+
+		s = Stopwatch.createStarted();
+		values.parallelStream().sorted().count();
+		LOG.info("T2: {} ms", s.elapsed(TimeUnit.MILLISECONDS));
+	}
+
+	public List<String> getUniqueList() {
+		int max = 1000000;
+		List<String> values = new ArrayList<>(max);
+		for (int i = 0; i < max; i++) {
+			UUID uuid = UUID.randomUUID();
+			values.add(uuid.toString());
+		}
+		return values;
+	}
+
 }
