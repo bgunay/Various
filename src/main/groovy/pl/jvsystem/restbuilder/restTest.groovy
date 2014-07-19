16469a56f11aba9050a4e5f5b2bb7c5b743b57dc
Przemek Nowak <przemyslaw.nowak@contractors.roche.com>
2014-07-19 14:40:56 +0200

    Added gradle build

--- src/main/groovy/pl/jvsystem/restbuilder/restTest.groovy
+++ src/main/groovy/pl/jvsystem/restbuilder/restTest.groovy
@@ -1,5 +1,6 @@
 package pl.jvsystem.restbuilder
 
+import groovyx.net.http.ContentType
 import groovyx.net.http.RESTClient
 
 //def twitter = new RESTClient( 'https://api.twitter.com/1.1/statuses/' )
@@ -13,7 +14,19 @@ import groovyx.net.http.RESTClient
 //println '--------------'
 //println resp.data
 
-def str = new RESTClient( 'http://localhost:8080/stranalysis/' )
-def resp = str.get( path : 'app/version.json' )
-println '--------------'
-println "Version $resp.data.version"
+//def str = new RESTClient( 'http://localhost:8080/stranalysis/' )
+//def resp = str.get( path : 'app/version.json' )
+//println '--------------'
+//println "Version $resp.data.version"
+
+def sampleHubRest = new RESTClient('http://resdev.gene.com/samplehub-rest/api/', ContentType.JSON)
+def wsPath = "v1/sample/639389,639390"
+
+def resp = sampleHubRest.get(path: wsPath)
+
+resp.data.content.each { sampleResp ->
+	println sampleResp.id.toLong()
+	sampleResp.attributes.each {
+		println "\t$it.columnName"
+	}
+}
--- src/main/java/pl/jvsystem/http/httpclient/Example.java
+++ src/main/java/pl/jvsystem/http/httpclient/Example.java
@@ -22,34 +22,34 @@ public class Example {
 
 	public static void main(String[] args) throws IOException {
 
-		HttpClient httpClient = HttpClientBuilder.create().build();
-		HttpPost post = new HttpPost(STR_ANALYSIS_URL);
-		MultipartEntity entity = MultipartEntityBuilder.create().build();
-
-		File f = new File("C:\\Dropbox\\Query_Template.xlsx");
-		ByteArrayBody body = new ByteArrayBody(IOUtils.toByteArray(new FileInputStream(f)), f.getName());
-		entity.addPart("file",body);
-		entity.addPart("fileType", new StringBody("T", ContentType.DEFAULT_TEXT));
-
-		entity.addPart("user", new StringBody("power", ContentType.DEFAULT_TEXT));
-
-		entity.addPart("trackingId", new StringBody("116c", ContentType.DEFAULT_TEXT));
-		entity.addPart("Amel", new StringBody("X", ContentType.DEFAULT_TEXT));
-		entity.addPart("CSF1PO", new StringBody("11", ContentType.DEFAULT_TEXT));
-		entity.addPart("D13S317", new StringBody("8", ContentType.DEFAULT_TEXT));
-		entity.addPart("D16S539", new StringBody("11", ContentType.DEFAULT_TEXT));
-		entity.addPart("D5S818", new StringBody("9", ContentType.DEFAULT_TEXT));
-		entity.addPart("D7S820", new StringBody("8", ContentType.DEFAULT_TEXT));
-		entity.addPart("TH01", new StringBody("6", ContentType.DEFAULT_TEXT));
-		entity.addPart("TPOX", new StringBody("8", ContentType.DEFAULT_TEXT));
-		entity.addPart("vWA", new StringBody("18", ContentType.DEFAULT_TEXT));
-
-		post.setEntity(entity);
-
-
-		HttpResponse response = httpClient.execute(post);
-		File nf = new File("C:\\Dropbox\\response.xlsx");
-		IOUtils.copy(response.getEntity().getContent(), new FileOutputStream(nf));
+//		HttpClient httpClient = HttpClientBuilder.create().build();
+//		HttpPost post = new HttpPost(STR_ANALYSIS_URL);
+//		MultipartEntity entity = MultipartEntityBuilder.create().build();
+//
+//		File f = new File("C:\\Dropbox\\Query_Template.xlsx");
+//		ByteArrayBody body = new ByteArrayBody(IOUtils.toByteArray(new FileInputStream(f)), f.getName());
+//		entity.addPart("file",body);
+//		entity.addPart("fileType", new StringBody("T", ContentType.DEFAULT_TEXT));
+//
+//		entity.addPart("user", new StringBody("power", ContentType.DEFAULT_TEXT));
+//
+//		entity.addPart("trackingId", new StringBody("116c", ContentType.DEFAULT_TEXT));
+//		entity.addPart("Amel", new StringBody("X", ContentType.DEFAULT_TEXT));
+//		entity.addPart("CSF1PO", new StringBody("11", ContentType.DEFAULT_TEXT));
+//		entity.addPart("D13S317", new StringBody("8", ContentType.DEFAULT_TEXT));
+//		entity.addPart("D16S539", new StringBody("11", ContentType.DEFAULT_TEXT));
+//		entity.addPart("D5S818", new StringBody("9", ContentType.DEFAULT_TEXT));
+//		entity.addPart("D7S820", new StringBody("8", ContentType.DEFAULT_TEXT));
+//		entity.addPart("TH01", new StringBody("6", ContentType.DEFAULT_TEXT));
+//		entity.addPart("TPOX", new StringBody("8", ContentType.DEFAULT_TEXT));
+//		entity.addPart("vWA", new StringBody("18", ContentType.DEFAULT_TEXT));
+//
+//		post.setEntity(entity);
+//
+//
+//		HttpResponse response = httpClient.execute(post);
+//		File nf = new File("C:\\Dropbox\\response.xlsx");
+//		IOUtils.copy(response.getEntity().getContent(), new FileOutputStream(nf));
 //		BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
 //		String line;
 //		while((line = rd.readLine()) != null) {
