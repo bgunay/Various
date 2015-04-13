81c9cba5b83745d1e37dbea68b42279d607361ba
Przemek Nowak <przemek.nowak.pl@gmail.com>
2015-04-13 08:55:04 +0200

    Reformatting

--- src/main/java/pl/jvsystem/generic/ListProcessor.java
+++ src/main/java/pl/jvsystem/generic/ListProcessor.java
@@ -3,43 +3,37 @@ package pl.jvsystem.generic;
 import java.util.LinkedList;
 import java.util.List;
 
-public abstract class ListProcessor<T>
-{
+public abstract class ListProcessor<T> {
 
-	public List<T> process(List<T> inputList)
-	{
-		List<T> newList = new LinkedList<T>();
+    public List<T> process(List<T> inputList) {
+        List<T> newList = new LinkedList<>();
 
-		for (T e : inputList)
-			newList.add(doProcess(e));
+        for (T e : inputList) {
+            newList.add(doProcess(e));
+        }
 
-		return newList;
-	}
+        return newList;
+    }
 
-	protected abstract T doProcess(T t);
+    protected abstract T doProcess(T t);
 }
 
-class TestListProcessor
-{
+class TestListProcessor {
 
-	public static void main(String... strings)
-	{
-		List<Integer> list = new LinkedList<Integer>();
-		list.add(1);
-		list.add(2);
+    public static void main(String... strings) {
+        List<Integer> list = new LinkedList<Integer>();
+        list.add(1);
+        list.add(2);
 
-		ListProcessor<Integer> lp = new ListProcessor<Integer>()
-		{
-			@Override
-			protected Integer doProcess(Integer t)
-			{
-				return t * 2;
-			}
+        ListProcessor<Integer> lp = new ListProcessor<Integer>() {
+            @Override
+            protected Integer doProcess(Integer t) {
+                return t * 2;
+            }
+        };
 
-		};
-
-		lp.process(list);
-		System.out.println(list);
-	}
+        lp.process(list);
+        System.out.println(list);
+    }
 
 }
