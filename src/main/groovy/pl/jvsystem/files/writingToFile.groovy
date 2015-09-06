770882273184c759a5471221a339d688598a3a8d
Przemek Nowak <przemek.nowak.pl@gmail.com>
2015-09-06 13:20:47 +0200

    Generation of test file by groovy

--- src/main/groovy/pl/jvsystem/files/writingToFile.groovy
+++ src/main/groovy/pl/jvsystem/files/writingToFile.groovy
@@ -1,12 +1,16 @@
 package pl.jvsystem.files
 
-import pl.jvsystem.domain.Person
-
-File file = new File('c:/TMP/output.txt')
+File file = new File('D:\\samples\\jhipster-test\\src\\main\\resources\\config\\liquibase\\bikes.csv')
 //file.text = "Ala ma kota"
 //file.bytes = [65, 66, 76, 68] as byte[]
-file << "Append New line"
+//file << "Append New line"
 
-file.withWriter { Writer writer ->
+def ln = System.getProperty('line.separator')
 
+file.withPrintWriter { PrintWriter writer ->
+    writer.write("id;name${ln}")
+    80.times { index ->
+        writer.write("${index+1};"+UUID.randomUUID().toString().substring(0,8)+"${ln}")
+    }
+    writer.flush()
 }
